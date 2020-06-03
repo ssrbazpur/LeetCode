51ec7997998e5fa1b5165ab822ddd10d90eb7a97
Simranjeet Singh Randhawa <ssrbazpur@gmail.com>
2020-06-03 11:15:57 -0600

    Create (Comparactor) Two City Scheduling.cpp

---
+++ June/(Comparactor) Two City Scheduling.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        //Comparator
+        sort(costs.begin(),costs.end(),
+             [](vector<int> a,vector<int> b){
+            return a[0]-a[1] < b[0]-b[1]; 
+        });
+        
+        int ans=0;
+        for(int i=0;i<costs.size()/2;i++){
+            ans+=costs[i][0];
+        }
+        for(int i=costs.size()/2;i<costs.size();i++){
+            ans+=costs[i][1];
+        }
+        return ans;
+        
+        
+    }
+};
