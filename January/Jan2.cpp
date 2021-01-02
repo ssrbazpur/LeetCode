d3743611367ff2f23a3c7457da2e22289947ec06
ssrbazpur <ssrbazpur@gmail.com>
2021-01-02 10:40:19 -0600

    :tada: Add Jan 2

---
+++ January/Jan2.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution
+{
+private:
+    // TreeNode *ans=NULL;
+public:
+    //INORDER Traversal
+    //     void dfs(TreeNode* original, TreeNode* cloned, TreeNode* target){
+    //         if(original==NULL){
+    //             return;
+    //         }
+    //         dfs(original->left,cloned->left,target);
+    //         if(original==target){
+    //             ans=cloned;
+    //         }
+    //         dfs(original->right,cloned->right,target);
+    //     }
+
+    TreeNode *getTargetCopy(TreeNode *original, TreeNode *cloned, TreeNode *target)
+    {
+        // dfs(original,cloned,target);
+        // return ans;
+
+        //Second Method
+        if (original == NULL)
+            return NULL;
+
+        if (original == target)
+        {
+            return cloned;
+        }
+
+        TreeNode *left = getTargetCopy(original->left, cloned->left, target);
+        if (left != NULL)
+            return left;
+        TreeNode *right = getTargetCopy(original->right, cloned->right, target);
+        return right;
+    }
+};
--- January/Notes.md
+++ January/Notes.md
@@ -2,5 +2,6 @@
 
 ## Week 1
 
-* Jan1: HashMap Use Easy
+* Jan 1: Easy: HashMap Use. First Value of arr as key and rest as values
+* Jan 2: Easy: Simple Inorder Traversal of two tree. Return node if it is equal to target 
  
