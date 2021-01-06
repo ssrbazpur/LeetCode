613c0195414438fc276cc0db49ee37ab41d7333a
ssrbazpur <ssrbazpur@gmail.com>
2021-01-06 12:08:03 -0600

    :tada: Jan6 Binary Search

--- January/Week1/Jan6.cpp
+++ January/Week1/Jan6.cpp
@@ -1,24 +1,40 @@
 class Solution
 {
 public:
-    //Space:O(1) | Time:O(N)
+    //Space:O(1) | Time:O(logN)
     int findKthPositive(vector<int> &arr, int k)
     {
 
-        int start = 1;
-        for (int i = 0; i < arr.size(); i++)
+        int left = 0;
+        int right = arr.size(); //Tricky Line
+
+        while (left < right)
         {
 
-            while (start != arr[i])
+            int mid = left + (right - left) / 2;
+            if (arr[mid] - (mid + 1) >= k)
             {
-                k--;
-                if (k == 0)
-                    return start;
-                start++;
+                right = mid;
             }
-            start++;
+            else
+            {
+                left = mid + 1;
+            }
         }
+        cout << left << " " << right;
 
-        return start + k - 1;
+        return right + k;
+        //         int start=1;
+        //         for(int i=0;i<arr.size();i++){
+
+        //             while(start!=arr[i]){
+        //                 k--;
+        //                 if(k==0) return start;
+        //                 start++;
+        //             }
+        //             start++;
+        //         }
+
+        //         return start+k-1;
     }
 };
