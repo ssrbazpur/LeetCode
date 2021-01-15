76a1ae160e8057acc37bc6c571a159171ec88651
ssrbazpur <ssrbazpur@gmail.com>
2021-01-15 14:32:26 -0600

    :sparkles: Week 3 Start

---
+++ January/Week3/Jan15.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    int getMaximumGenerated(int n)
+    {
+        vector<int> nums(n + 1, 0);
+
+        int ans = 0;
+
+        if (n >= 1)
+        {
+            ans = 1;
+            nums[1] = 1;
+        }
+
+        for (int i = 1; 2 * i + 1 <= n; i++)
+        {
+            nums[2 * i] = nums[i];
+            nums[2 * i + 1] = nums[i] + nums[i + 1];
+        }
+
+        for (int n : nums)
+        {
+            cout << n;
+            ans = max(ans, n);
+        }
+
+        return ans;
+    }
+};
