8e79ea8ce161d2dcd53a39f57a8cb1b02d5877ad
ssrbazpur <ssrbazpur@gmail.com>
2021-01-20 12:40:28 -0600

    :tada: Jan20

---
+++ January/Week3/Jan20.cpp
@@ -0,0 +1,39 @@
+class Solution
+{
+public:
+    bool isValid(string s1)
+    {
+        stack<char> s;
+        bool ans = true;
+        for (char a : s1)
+        {
+            if (s.empty())
+            {
+                s.push(a);
+            }
+            else
+            {
+                char t = s.top();
+                if (a == ']' && t == '[')
+                {
+                    cout << "heloo";
+                    s.pop();
+                }
+                else if (a == ')' && t == '(')
+                {
+                    s.pop();
+                }
+                else if (a == '}' && t == '{')
+                {
+                    s.pop();
+                }
+                else
+                {
+                    s.push(a);
+                }
+            }
+        }
+
+        return s.empty();
+    }
+};
